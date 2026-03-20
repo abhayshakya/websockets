9a1085ace19281e3c987adf11f2a00210b271e2e
abhayshakya <shabhay18@gmail.com>
2026-03-20 12:23:21 +0545

    server.js

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ import express from "express";
 import http from "http";
 import { WebSocketServer } from "ws";
 import { setupWebSocket } from "./src/routes/webSocket.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
-
 const wss = new WebSocketServer({ server });
 
+// Serve frontend folder as static
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// Send index.html for root
 app.get("/", (req, res) => {
-  res.send("Server running");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
+// Setup WebSocket
 setupWebSocket(wss);
 
+// Start server
 server.listen(8000, () => {
   console.log("HTTP + WS running on http://localhost:8000");
 });
