f1867174091c185076de1301b12633782d91a2f4
abhayshakya <shabhay18@gmail.com>
2026-03-30 14:31:02 +0545

    validations

--- src/routes/login.js
+++ src/routes/login.js
@@ -10,7 +10,6 @@ router.post('/', async (req, res) => {
   if(!username || !password) {
     return res.json({ success: false, message: "Username and password required"});
   }
-  
   const hash = users.get(username);
   if(!hash) {
     return res.json({ success: false, message:"invalid credentials"})
@@ -18,7 +17,7 @@ router.post('/', async (req, res) => {
 
   const match = await bcrypt.compare(password, hash);
   if (!match) {
-    return res.json({ success: false, message: "invalid credentials" });
+    return res.json({ success: false, message: "Password is incorrect" });
   }
 
   const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
--- src/routes/register.js
+++ src/routes/register.js
@@ -4,17 +4,28 @@ import { users } from '../../server.js';
 
 const router = express.Router();
 
-router.post('/', async (req, res) =>{
-  const {username, password} = req.body;
-  if(!username || !password) {
-    return res.json({ success: false, message: "Username and Password is required"})
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.json({ success: false, message: "Username and Password is required" })
   }
-  if(users.has(username)) {
-    return res.json({ success: false, message: "User already exists"})
+
+  const existingUSer = users.get(username);
+  if (existingUSer) {
+    return res.json({ success: false, message: "Username already exists" });
   }
+
+  if (users.has(username)) {
+    return res.json({ success: false, message: "User already exists" })
+  }
+
+  if (typeof password !== 'string' || password.length < 6) {
+    return res.json({ success: false, message: "Password must be a string and at least 6 characters long" });
+  }
+
   const hashed = await bcrypt.hash(password, 10)
   users.set(username, hashed);
-  res.json({ success: true, message: "User Registered Successfully"});
+  res.json({ success: true, message: "User Registered Successfully" });
 });
 
 export default router;
